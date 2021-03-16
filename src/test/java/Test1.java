f8e1e69ee494f5242581f89e94129c15c592d045
frb <frb15572873759@163.com>
2021-03-16 19:26:30 +0800

    修改了测试类

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -5,5 +5,9 @@
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("sss");
+        System.out.println("sss");
+        System.out.println("sss");
+        System.out.println("sss");
+
     }
 }

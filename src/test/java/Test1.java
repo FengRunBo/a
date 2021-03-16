cc55d0d8874876bee75c1a550fa0b640a7ab85b0
frb <frb15572873759@163.com>
2021-03-16 19:22:22 +0800

    添加了测试类

---
+++ src/test/java/Test1.java
@@ -0,0 +1,9 @@
+/**
+ * @author Procedural apes with hair
+ * @create 2021-03-16-19:15
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("sss");
+    }
+}
